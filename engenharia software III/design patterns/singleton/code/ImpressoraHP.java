1e1eb74bd47fa92524a8b5b48ec72ab48be3769f
Laiza C. M. Z. Truyts <111503805+LaizaCristina@users.noreply.github.com>
2023-09-18 10:45:32 -0300

    Update ImpressoraHP.java

--- engenharia software III/design patterns/singleton/code/ImpressoraHP.java
+++ engenharia software III/design patterns/singleton/code/ImpressoraHP.java
@@ -1,14 +1,14 @@
 public static class ImpressoraHP {
 	private static ImpressoraHP impressoraX;
 	private ImpressoraHP(){}
-	public ImpressoraX getInstance(){
+	public void ImpressoraHP getInstance(){
 		if(impressoraHP==null){
 			impressoraHP = new ImpressoraHP;
 		}
 		return impressoraHP;
 	}
 
-	public void imprimir(String documento) {
+	public imprimir(String documento) {
         // Lógica de impressão aqui.
         System.out.println("Imprimindo: " + documento);
     }
